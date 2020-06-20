aca555db86e994a7a13c41dc9925e5fb2efb440b
zaida12360 <zaida1938@hotmail.com>
2020-06-20 18:16:36 -0500

    Primer mapa

---
+++ data/Concesiones_mineras_6.js
@@ -0,0 +1,24 @@
+var json_Concesiones_mineras_6 = {
+"type": "FeatureCollection",
+"name": "Concesiones_mineras_6",
+"crs": { "type": "name", "properties": { "name": "urn:ogc:def:crs:OGC:1.3:CRS84" } },
+"features": [
+{ "type": "Feature", "properties": { "CONCESION": "LA VEGA", "X": 680000.0, "Y": 9557200.0, "SUPERFICIE": 4.0, "CANTON": "CATAMAYO", "MINERAL": "ARCILLA" }, "geometry": { "type": "Point", "coordinates": [ -79.378668758004892, -4.0044756749779 ] } },
+{ "type": "Feature", "properties": { "CONCESION": "16 DE ABRIL", "X": 680200.0, "Y": 9556500.0, "SUPERFICIE": 4.0, "CANTON": "CATAMAYO", "MINERAL": "ARCILLA" }, "geometry": { "type": "Point", "coordinates": [ -79.376855282330254, -4.010802348885916 ] } },
+{ "type": "Feature", "properties": { "CONCESION": "ARCIMEGO 2", "X": 665000.0, "Y": 9559600.0, "SUPERFICIE": 18.0, "CANTON": "CATAMAYO", "MINERAL": "ARCILLA" }, "geometry": { "type": "Point", "coordinates": [ -79.51378670409909, -3.983027508101319 ] } },
+{ "type": "Feature", "properties": { "CONCESION": "VALLE HERMOSO", "X": 679600.0, "Y": 9556600.0, "SUPERFICIE": 3.0, "CANTON": "CATAMAYO", "MINERAL": "ARCILLA" }, "geometry": { "type": "Point", "coordinates": [ -79.382260083210454, -4.009908761342338 ] } },
+{ "type": "Feature", "properties": { "CONCESION": "COMPAÑÍA 1", "X": 680100.0, "Y": 9556300.0, "SUPERFICIE": 4.0, "CANTON": "CATAMAYO", "MINERAL": "ARCILLA" }, "geometry": { "type": "Point", "coordinates": [ -79.377752216566279, -4.012612784536833 ] } },
+{ "type": "Feature", "properties": { "CONCESION": "COMPAÑÍA 5", "X": 678700.0, "Y": 9557000.0, "SUPERFICIE": 4.0, "CANTON": "CATAMAYO", "MINERAL": "ARCILLA" }, "geometry": { "type": "Point", "coordinates": [ -79.390371709960533, -4.006307484785281 ] } },
+{ "type": "Feature", "properties": { "CONCESION": "ARCIMEGO 1", "X": 666500.0, "Y": 9559000.0, "SUPERFICIE": 12.0, "CANTON": "CATAMAYO", "MINERAL": "ARCILLA" }, "geometry": { "type": "Point", "coordinates": [ -79.500268948500263, -3.988429202254364 ] } },
+{ "type": "Feature", "properties": { "CONCESION": "LOS TEJARES", "X": 679900.0, "Y": 9557000.0, "SUPERFICIE": 4.0, "CANTON": "CATAMAYO", "MINERAL": "ARCILLA" }, "geometry": { "type": "Point", "coordinates": [ -79.379565695760746, -4.006286107719025 ] } },
+{ "type": "Feature", "properties": { "CONCESION": "INDIUCHO", "X": 681700.0, "Y": 9554700.0, "SUPERFICIE": 4.0, "CANTON": "CATAMAYO", "MINERAL": "ARCILLA" }, "geometry": { "type": "Point", "coordinates": [ -79.363315330905948, -4.027053025156553 ] } },
+{ "type": "Feature", "properties": { "CONCESION": "VILLONACO", "X": 692900.0, "Y": 9558600.0, "SUPERFICIE": 4.0, "CANTON": "CATAMAYO", "MINERAL": "ARCILLA" }, "geometry": { "type": "Point", "coordinates": [ -79.262534838003589, -3.99157706993085 ] } },
+{ "type": "Feature", "properties": { "CONCESION": "COOP: 27 DE JUNIO", "X": 680200.0, "Y": 9557000.0, "SUPERFICIE": 4.0, "CANTON": "CATAMAYO", "MINERAL": "ARCILLA" }, "geometry": { "type": "Point", "coordinates": [ -79.376864201308237, -4.006280741127478 ] } },
+{ "type": "Feature", "properties": { "CONCESION": "COMPAÑÍA 2", "X": 679300.0, "Y": 9556800.0, "SUPERFICIE": 4.0, "CANTON": "CATAMAYO", "MINERAL": "ARCILLA" }, "geometry": { "type": "Point", "coordinates": [ -79.384965147009069, -4.008105464556933 ] } },
+{ "type": "Feature", "properties": { "CONCESION": "SAN VICENTE II", "X": 662500.0, "Y": 9567600.0, "SUPERFICIE": 22.0, "CANTON": "CATAMAYO", "MINERAL": "CALIZA" }, "geometry": { "type": "Point", "coordinates": [ -79.536426792473861, -3.910716651116728 ] } },
+{ "type": "Feature", "properties": { "CONCESION": "EL PUCARA", "X": 678900.0, "Y": 9555800.0, "SUPERFICIE": 6.0, "CANTON": "CATAMAYO", "MINERAL": "ARCILLA" }, "geometry": { "type": "Point", "coordinates": [ -79.38854943531615, -4.017155852624812 ] } },
+{ "type": "Feature", "properties": { "CONCESION": "ARCIMEGO 3", "X": 666700.0, "Y": 9558600.0, "SUPERFICIE": 84.0, "CANTON": "CATAMAYO", "MINERAL": "ARCILLA" }, "geometry": { "type": "Point", "coordinates": [ -79.498461316170449, -3.992043405086894 ] } },
+{ "type": "Feature", "properties": { "CONCESION": "ARCIMEGO 4", "X": 665100.0, "Y": 9559400.0, "SUPERFICIE": 82.0, "CANTON": "CATAMAYO", "MINERAL": "ARCILLA" }, "geometry": { "type": "Point", "coordinates": [ -79.512882924746691, -3.984834639291561 ] } },
+{ "type": "Feature", "properties": { "CONCESION": "PALTAS", "X": 630900.0, "Y": 9543800.0, "SUPERFICIE": 4.0, "CANTON": "PALTAS", "MINERAL": "CALIZA" }, "geometry": { "type": "Point", "coordinates": [ -79.820680730691095, -4.126435814544908 ] } }
+]
+}
---
+++ data/Indicios_mineralgicos_5.js
@@ -0,0 +1,10 @@
+var json_Indicios_mineralgicos_5 = {
+"type": "FeatureCollection",
+"name": "Indicios_mineralgicos_5",
+"crs": { "type": "name", "properties": { "name": "urn:ogc:def:crs:OGC:1.3:CRS84" } },
+"features": [
+{ "type": "Feature", "properties": { "MINERAL": "CALIZA", "X": 587900.0, "Y": 9561000.0 }, "geometry": { "type": "Point", "coordinates": [ -80.208201404680651, -3.971326320972572 ] } },
+{ "type": "Feature", "properties": { "MINERAL": "CALIZA", "X": 601100.0, "Y": 9570500.0 }, "geometry": { "type": "Point", "coordinates": [ -80.089399135207231, -3.885268948598207 ] } },
+{ "type": "Feature", "properties": { "MINERAL": "ESMECTITA", "X": 660000.0, "Y": 9573000.0 }, "geometry": { "type": "Point", "coordinates": [ -79.559021368420744, -3.861918060149637 ] } }
+]
+}
